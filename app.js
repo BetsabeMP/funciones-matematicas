9ddbd2d39ac23c915fbdcf3cd406238195910bd7
Betsabé Mique <betsabe.mique.p@gmail.com>
2018-05-28 08:56:37 -0400

    suma y multipliación realizados

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+const add = (number2, number3) => number2 + number3;
+//La funcion multiply debe multiplicar sus argumentos. 
+const multiply = (add, number1) => add * number1;
+//Usando solamente estas dos funciones, resuelva este simple problema matemático: 36325 * (9824 + 777).
+
+let result1 = add(9824, 777);
+let result2 = multiply(36325, result1);
+
+console.log(result2); */
+
+
+
+function add (number_1,number_2) {
+    number_1 + number_2
+}
+
+add (9824,777)
+
+function multiply (number_3,number_4) {
+    console.log (number_3 * number_4)
+}
+
+multiply (36325,10601)
+
+
+
